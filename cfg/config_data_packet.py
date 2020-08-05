62ffccd0e68c229bb592c3b2c417fa34ac71a75e
Plutaniano <lucasrodriguessilva@hotmail.com>
2020-08-05 12:55:34 -0300

    better file parsing

--- cfg/config_data_packet.py
+++ cfg/config_data_packet.py
@@ -3,20 +3,20 @@ from parameter import Parameter
 class Config_Data_Packet:
     all = []
 
-    def __init__(self, length, rawbytes):
+    def __init__(self, length, packet_id, param_count, parameters_raw):
         self.all.append(self)
         self.length = length
-        self.id = rawbytes[0]
-        self.param_count = int.from_bytes(rawbytes[1:3], 'little')
-        parameters = []
-        while rawbytes:
-            param_id = int.from_bytes(rawbytes[:2], 'little')
-            param_length = int.from_bytes(rawbytes[2:3], 'little')
-            param_value = int.from_bytes(rawbytes[3:3+param_length], 'little')
+        self.id = packet_id
+        self.param_count = param_count
+        self.parameters = []
+        while parameters_raw:
+            param_id = int.from_bytes(parameters_raw[:2], 'little')
+            param_length = int.from_bytes(parameters_raw[2:3], 'little')
+            param_value = parameters_raw[3:3+param_length]
             p = Parameter(param_id, param_length, param_value)
-            parameters.append(p)
-            rawbytes = rawbytes[3+param_length:]
-        self.data = parameters
+            self.parameters.append(p)
+            parameters_raw = parameters_raw[2+1+param_length:]
+        
     
     def __repr__(self):
         return f'[DataPacket] <ID:{self.id} params:{self.param_count}>'
--- cfg/config_file.py
+++ cfg/config_file.py
@@ -1,15 +1,21 @@
 from config_data_packet import Config_Data_Packet
 from parameter import Parameter
+import os
 
 class Config_File:
     def __init__(self, filepath):
         self.data_packets = []
         with open(filepath, 'rb') as f:
-            length = int.from_bytes(f.read(2), 'little')
-            while length != 0:
-                p = Config_Data_Packet(length, f.read(length-2))
-                self.data_packets.append(p)
+            eof = f.seek(0, os.SEEK_END)
+            f.seek(0)
+            while f.tell() != eof:
                 length = int.from_bytes(f.read(2), 'little')
+                packet_id = int.from_bytes(f.read(1), 'little')
+                param_count = int.from_bytes(f.read(2), 'little')
+                params = f.read(length-5)
+                p = Config_Data_Packet(length, packet_id, param_count, params)
+                print(p)
+                self.data_packets.append(p)
 
     def __repr__(self):
         param_count = 0
